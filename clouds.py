3476f285de7546621fbdbb18b89855d0a795e563
Astitva Gupta <gupta.astitva@gmail.com>
2020-01-12 19:07:29 +0530

    Mode major changes

--- clouds.py
+++ clouds.py
@@ -1,5 +1,38 @@
 from random import randint
 import os
+# from main import add_element
+block = "\u2588"
+board_len = 4000
+
+
+
+def add_element(temp_list=None,cont=-1,fn=-1,bn=-1,fx=-1,r1=0,g1=0,b1=0,bx=-1,r2=0,g2=0,b2=0,element=block,end=""):
+    f=False
+    if(temp_list==None):
+        f=True
+        temp_list = list()
+    if cont == 1:
+        temp_list.append(["","",element,""])
+        return
+    if fn != -1:
+        if bn==-1 and bx==-1:
+            bn=fn+10
+            temp_list.append(["\x1B["+str(fn)+"m","\x1B["+str(bn)+"m",element,end])
+        else:
+            temp_list.append(["\x1B["+str(fn)+"m","\x1b[48;2;"+str(r2)+";"+str(g2)+";"+str(b2)+"m",element,end])
+
+    else:
+        if bx!=-1:
+            temp_list.append(["\x1b[38;2;"+str(r1)+";"+str(g1)+";"+str(b1)+"m",
+                              "\x1b[48;2;"+str(r2)+";"+str(g2)+";"+str(b2)+"m",
+                              element,end])
+        else:
+            temp_list.append(["\x1b[38;2;"+str(r1)+";"+str(g1)+";"+str(b1)+"m",
+                              "\x1B["+str(bn)+"m",element,end])
+    if(f==True):
+        return temp_list
+            
+
 rows, columns = os.popen('stty size', 'r').read().split()
 block = "\u2588"
 
@@ -23,13 +56,14 @@ small_cloud = [[0,0,0,1,1,1,0,0],
 #         print(reset_color)
 
 def fill_in_art(board,freq,art):
-    for p in range(3000//freq):
+    for p in range(board_len//freq):
         starty=randint(3,freq-3-len(art[0]))+p*freq
         startx=randint(3,int(rows)-3-len(art))
-        print("",end="",flush=True)
         i=0
         j=0
         for i in range(len(art)):
             for j in range(len(art[0])):
                 if art[i][j]==1:
-                    board[startx+i][starty+j]="\x1B[37m"+block+"\x1B[36m"
+                    board[startx+i][starty+j]=add_element(fn=37,end="\x1B[36m\x1B[46m")[0]
+                    # print(add_element(fn=37,end="\x1B[36m\x1B[46m"))
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os, time, threading
-from clouds import fill_in_art,small_cloud,large_cloud
+from clouds import fill_in_art,small_cloud,large_cloud,add_element,board_len
 from rider import *
 
 rows, columns = os.popen('stty size', 'r').read().split()
@@ -8,24 +8,14 @@ block = "\u2588"
 board = list()
 
 
-
-# def print_rider(art):
-#     for i in range(len(art)):
-#         for j in range(len(art[i])): 
-#             print(art[i][j],end="")
-#         print(reset_color)
-
 def print_board1(board,board_start):
+    print("",end="\033[0;0f")
     for i in range(int(rows)):
         for j in range(int(columns)):
-            rx = Rider()
-            if (i not in rx.art_areay) or (j not in rx.art_areax):
-                print(board[i][j+board_start],end="")
-            else:
-                print("\033[1C",end="")
-
-            # if i  not in Rider.art_areay and j not in Rider.art_areax:
-            # print(board[i][j+board_start],end="")
+            val = ""
+            for x in board[i][j+board_start]:
+                val+=x
+            print(val,end="")
         if i!=int(rows)-1 and j!=int(columns)-1:
             print()
         else:
@@ -42,20 +32,18 @@ def print_board(board,board_start):
                 print(board[i][j+board_start],end="")
             else:
                 print("\033[1C",end="")
-
-            # if i  not in Rider.art_areay and j not in Rider.art_areax:
-            # print(board[i][j+board_start],end="")
         if i!=int(rows)-1 and j!=int(columns)-1:
             print()
         else:
             print("",end="")
     print(reset_color,end="",flush=True)
 
+
 def create_board():
     board = list()
     for i in range(int(rows)):
         temp_list = list()
-        for j in range(3000):
+        for j in range(board_len):
             if(i==0):
                 temp_list.append("\x1B[34m\x1B[44m"+block)
             elif(i==1):
@@ -69,63 +57,51 @@ def create_board():
     fill_in_art(board,100,large_cloud)
     return board
 
-def add_element(temp_list,fn=-1,bn=-1,fx=-1,r1=0,g1=0,b1=0,bx=-1,r2=0,g2=0,b2=0,element=block):
-    if fn != -1:
-        if bn==-1:
-            bn=fn+10
-        temp_list.append(["\x1B["+fn+"m","\x1B["+bn+"m",block])
-    else:
-        temp_list.append([""])
-            
+
 
 def create_board1():
     board = list()
     for i in range(int(rows)):
         temp_list = list()
-        for j in range(3000):
+        for j in range(board_len):
             if(i==0):
                 add_element(temp_list,fn=34)
-                # temp_list.append("\x1B[34m\x1B[44m"+block)
             elif(i==1):
                 add_element(temp_list,fn=36)
-                # temp_list.append("\x1B[36m\x1B[46m"+block)
             elif(i==int(rows)-1):
                 add_element(temp_list,fn=32)
-                # temp_list.append("\x1B[32m\x1B[42m"+block)
             else:
-                temp_list.append(block)
+                add_element(temp_list,cont=1)
         board.append(temp_list)
     fill_in_art(board,30,small_cloud)
     fill_in_art(board,100,large_cloud)
     return board
 
 
-# def print_rider(xstart=(int(columns)*10)//100,ystart=int(rows/2)):
-
-
-
-
-
 
 def increase_strt(board_start=0):
-    while(board_start+int(columns)<=len(board[0])):
-        print("\033[0;0f",end="")
-        print_board(board,board_start)
-        time.sleep(0.01)
+    # print_board1(board,board_start)
+    while(board_start+int(columns)<=board_len):
+        print_board1(board,board_start)
+        time.sleep(0.00001)
         board_start+=1
 
             
 if __name__=="__main__":
     # print_rider(rider)
-    board = create_board()
-    for i in range(int(rows)):
-        board[i][2999]="\x1B[33m"+block+"\x1B[36m"
+    # board = create_board()
+    # for i in range(int(rows)):
+    #     board[i][2999]="\x1B[33m"+block+"\x1B[36m"
+    #     board[i][2999]=add_element(fn=33)
 
-    # print("\033[s",end="")
     
-    main_rider = Rider()
-    main_rider.print_rider()
-    # time.sleep(10)
+    # main_rider = Rider()
+    # main_rider.print_rider()
+    board = create_board1()
+    for i in range(int(rows)):
+        board[i][board_len-1]=add_element(fn=33,end="\x1B[36m")[0]
+        board[i][board_len-10]=add_element(fn=33,end="\x1B[36m")[0]
+        board[i][board_len-2]=add_element(fn=33,end="\x1B[36m")[0]
     thread = threading.Thread(target=increase_strt)
     thread.start()
 
