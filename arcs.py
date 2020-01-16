09bf6f9b9d8b9f7b243f806f962464a6db6db555
Astitva Gupta <gupta.astitva@gmail.com>
2020-01-16 17:33:46 +0530

    Corrected arcs

--- arcs.py
+++ arcs.py
@@ -1,29 +1,29 @@
 from random import randint
 import os
+from clouds import board_len
+
 reset_color="\x1B[0m"
-# from clouds import *
+rows, columns = os.popen('stty size', 'r').read().split()
+rows = int(rows)
 
 col_yf='\x1B[38;2;255;255;0m'
 col_of='\x1b[38;2;255;165;0m'
 col_ob='\x1b[48;2;255;165;0m'
 
-arc_h = [[col_of, '', '▂' , '', 1], [col_yf, col_ob, '▀' , '', -1]]
 arc_v = [[col_yf, col_ob, '▐' , '', 1], [col_of, '', '▌' , '', -1]]
-arc_r = [[col_yf, col_ob, '▗' , '', 1], [col_yf, col_ob, '▚' , '', 1], [col_yf, col_ob, '▘' , '', 1]]
-arc_l = [[col_yf, col_ob, '▖' , '', 1], [col_yf, col_ob, '▞' , '', 1], [col_yf, col_ob, '▝' , '', 1]]
+arc_h = [[col_of, '', '▂' , '', 1], [col_yf, col_ob, '▀' , '', -1]]
+arc_r = [
+            [['','',' ','',-1],[col_yf, col_ob, '▗' , '', 1],[col_of,'','▖','',-1]],
+            [[col_of,'','▝','',-1],[col_yf, col_ob, '▚' , '', 1],[col_of,'','▖','',-1]],
+            [[col_of,'','▝','',-1],[col_yf, col_ob, '▘' , '', 1],['','',' ','',-1]]
+        ]
+arc_l = [
+            [[col_of,'','▗','',-1],[col_yf, col_ob, '▖' , '', 1],['','',' ','',-1]],
+            [[col_of,'','▗','',-1],[col_yf, col_ob, '▞' , '', 1],[col_of,'','▘','',-1]],
+            [['','',' ','',-1],[col_yf, col_ob, '▝' , '', 1],[col_of,'','▘','',-1]]
+        ]
 
-# def fill_in_art1(board,freq,art):
-#     for p in range(board_len//freq):
-#         starty=randint(3,freq-3-len(art[0]))+p*freq
-#         startx=randint(3,int(rows)-3-len(art))
-#         i=0
-#         j=0
-#         for i in range(len(art)):
-#             for j in range(len(art[0])):
-#                 if art[i][j]==1:
-#                     board[startx+i][starty+j]=add_element(fn=37,end="\x1B[36m\x1B[46m")
-#                     print(add_element(fn=37,end="\x1B[36m\x1B[46m"))
-for j in range(10):
+for j in range(rows//2):
     for i in arc_v:
         val=""
         for x in i:
@@ -32,8 +32,10 @@ for j in range(10):
         print(val+reset_color,end="")
     print()
 print()
+
+
 for i in range(len(arc_h)):
-    for j in range(10):
+    for j in range(rows):
         val=""
         for x in arc_h[i]:
             if(type(x) != int):
@@ -42,41 +44,46 @@ for i in range(len(arc_h)):
     print()
 print()
 
-for j in range(10):
-    if j>0 and j<9: g=1
+
+
+for j in range(rows//2):
+    if j>0 and j<rows//2-1: g=1
     elif j==0: g=0
     else: g=2
     val=""
     for x in arc_r[g]:
-        if(type(x) != int):
-            val+=x
+        for p in x:
+            if(type(p) != int):
+                val+=p
+        val+=reset_color
     print(' '*j,end="")
-    print(val+reset_color)
+    print(val)
 print()
-for j in range(9,-1,-1):
-    if j>0 and j<9:
-        g=1
-    elif j==0:
-        g=2
-    else:
-        g=0
-        # print(' '*j,end='')
-    if j>0 and j<=9:
-        print(' '*(j-1)+col_of+'▗',end="")
+
+for j in range(rows//2-1,-1,-1):
+    if j>0 and j<rows//2-1: g=1
+    elif j==0: g=2
+    else: g=0
     val=""
     for x in arc_l[g]:
-        if(type(x) != int):
-            val+=x
-    # print(' '*j,end="")
-    if j>=0 and j<9:
-        val+=reset_color+col_of+'▘'
-    print(val+reset_color)
-# val=""
-# for x in arc_r[2]:
-#     if(type(x) != int):
-#         val+=x
-# print(" "+val+reset_color)
+        for p in x:
+            if(type(p) != int):
+                val+=p
+        val+=reset_color
+    print(' '*j,end="")
+    print(val)
+print()
 
-# for j in range(10):
-#     for i in arc_h:
-#         print(i+reset_color)
+
+def fill_in_art(board,freq,art):
+    for p in range(board_len//freq):
+        starty=randint(3,freq-3-len(art[0]))+p*freq
+        startx=randint(3,int(rows)-3-len(art))
+        i=0
+        j=0
+        for i in range(len(art)):
+            for j in range(len(art[0])):
+                if art[i][j]==1:
+                    pass
+                    # board[startx+i][starty+j]=(fn=37,end="\x1B[36m\x1B[46m")
+                    # print(add_element(fn=37,end="\x1B[36m\x1B[46m"))
